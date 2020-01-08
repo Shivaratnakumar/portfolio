2639606a43a1e26595349e19c44b3ea46c6e71c0
Shivaratnakumar <shivaratnakumarpatil@gmail.com>
2020-01-09 02:21:24 +0530

    Updates

---
+++ precache-manifest.4b347ae2fecd2f73d67b8b95674ba35f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93159dcf8fe84ef086271ca8339419a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8b12aa231d4550514e81",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "5b54d04a415037a98860",
+    "url": "/portfolio/static/css/main.fc676c8c.chunk.css"
+  },
+  {
+    "revision": "8b12aa231d4550514e81",
+    "url": "/portfolio/static/js/2.0c902369.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.0c902369.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b54d04a415037a98860",
+    "url": "/portfolio/static/js/main.1ec88764.chunk.js"
+  },
+  {
+    "revision": "e139b2548d94e481a5d0",
+    "url": "/portfolio/static/js/runtime-main.0762ecda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
